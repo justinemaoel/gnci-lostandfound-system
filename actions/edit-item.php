ff993bc3142ad20b411c0c4fb2cac7eb9921c37d
Justine Maoel <justinemaoel.gnc@gmail.com>
2026-05-07 15:38:15 +0800

    fixed edit

--- actions/edit-item.php
+++ actions/edit-item.php
@@ -1,7 +1,7 @@
 <?php
 
 session_start();
-require_once __DIR__ . '../includes/db.php';
+require_once __DIR__ . '/../includes/db.php';
 
 if (!isset($_SESSION['user_id']) || !isset($_POST['submit_edit'])) {
     header("Location: ../auth/login.php");
