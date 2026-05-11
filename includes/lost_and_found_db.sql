8c8052146cdb1edcb2e7db3d272f0f511b773ae4
Justine Maoel <justinemaoel.gnc@gmail.com>
2026-05-11 15:10:46 +0800

    updated

--- includes/lost_and_found_db.sql
+++ includes/lost_and_found_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 07, 2026 at 07:18 AM
+-- Generation Time: May 11, 2026 at 09:09 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -69,15 +69,22 @@ CREATE TABLE `items` (
   `contact_phone` varchar(50) NOT NULL,
   `contact_num` varchar(20) DEFAULT NULL,
   `date_reported` date NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `submitted_to_office` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `items`
 --
 
-INSERT INTO `items` (`id`, `user_id`, `category_id`, `item_name`, `location_text`, `description`, `notes`, `item_img`, `time_last_seen`, `status`, `post_type`, `upload_status`, `item_resolved_status`, `contact_email`, `contact_phone`, `contact_num`, `date_reported`, `created_at`) VALUES
-(14, 2, 1, 'test', 'test', 'test', 'test', '1778130405_Image.png', '13:08:00', 'available', 'Found', 'approved', 'not resolved', '', '', '', '2026-05-27', '2026-05-07 05:06:45');
+INSERT INTO `items` (`id`, `user_id`, `category_id`, `item_name`, `location_text`, `description`, `notes`, `item_img`, `time_last_seen`, `status`, `post_type`, `upload_status`, `item_resolved_status`, `contact_email`, `contact_phone`, `contact_num`, `date_reported`, `created_at`, `submitted_to_office`) VALUES
+(40, 2, 3, 'Brown Wallet', 'College Library', 'test', 'test', '1778480976_wallet.png', '15:31:00', 'approved', 'Found', 'approved', 'not resolved', 'test@gmail.com', '', '12345', '2026-05-12', '2026-05-11 06:29:36', 1),
+(41, 2, 4, 'Blue Umbrella', 'PH102', 'test', 'test', '1778481017_umbrella.png', '14:33:00', 'approved', 'Found', 'approved', 'not resolved', 'test@gmail.com', '', '12345', '2026-05-10', '2026-05-11 06:30:17', 0),
+(42, 2, 3, 'Student Id', 'School Canteen', 'test', 'test', '1778481049_student id.png', '14:32:00', 'approved', 'Lost', 'approved', 'not resolved', 'test@gmail.com', '', '12345', '2026-05-14', '2026-05-11 06:30:49', 0),
+(43, 2, 1, 'Earphones', 'High School Library', 'test', 'test', '1778481145_earphones.png', '14:34:00', 'approved', 'Lost', 'approved', 'not resolved', 'test@gmail.com', '', '12345', '2026-05-13', '2026-05-11 06:32:25', 0),
+(44, 2, 6, 'Aqua Flask Red Tumbler', 'GH108', 'TEST', 'TEST', '1778481234_tumbler.png', '15:35:00', 'approved', 'Found', 'approved', 'not resolved', 'test@gmail.com', '', '12345', '2026-05-11', '2026-05-11 06:33:54', 1),
+(45, 2, 1, 'Black Samsung', 'School Canteen', 'test', 'test', '1778481291_samsung.png', '08:34:00', 'approved', 'Found', 'approved', 'not resolved', 'test@gmail.com', '', '12345', '2026-05-07', '2026-05-11 06:34:51', 1),
+(46, 2, 6, 'Stethoscope', 'LH309', 'test', 'test', '1778481374_stethoscope.png', '14:38:00', 'approved', 'Lost', 'approved', 'not resolved', 'test@gmail.com', '', '12345', '2026-05-01', '2026-05-11 06:36:14', 0);
 
 -- --------------------------------------------------------
 
@@ -100,7 +107,9 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `password`, `role`) VALUES
 (2, 'Justine Maoel', 'Figura', 'maoeljustine@gmail.com', '$2y$10$kndavFWGiSufzUvndtDX0.IOoqbtg7gfoG/7wxMXdzfCL1rewPKJm', 'student'),
-(7, 'GNC', 'Admin', 'admin.lostandfound@gmail.com', '$2y$10$ySRjo/DmQeL7SCW3fP7upuxcbXNpx.UXB03FXNvbUMNA19E23De.y', 'admin');
+(7, 'GNC', 'Admin', 'admin.lostandfound@gmail.com', '$2y$10$ySRjo/DmQeL7SCW3fP7upuxcbXNpx.UXB03FXNvbUMNA19E23De.y', 'admin'),
+(8, 'Justine', 'Figura', 'justinefigura@gmail.com', '$2y$10$IeRl.dhgkomtGZ2f6K.EKeCuWnRN3fpMkvmmuEXXe0xJ2fpSwkNta', 'student'),
+(9, 'maoel', 'figura', 'maoelfigura@gmail.com', '$2y$10$Mqd7lHy2xN2Mpzi8/bXvU.SsDAgWlLIBPQWAJOam3r/DQrskb2BxG', 'student');
 
 --
 -- Indexes for dumped tables
@@ -140,13 +149,13 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Constraints for dumped tables
